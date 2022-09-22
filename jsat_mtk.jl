d565f8134f78d9ee08d306678f3c86ec87c5d7fa
Jonnie Diegelman <jonniediegelman@gmail.com>
2022-09-22 09:18:43 -0400

    Some fixes

--- jsat_mtk.jl
+++ jsat_mtk.jl
@@ -1,5 +1,5 @@
 using ModelingToolkit, ModelingToolkitStandardLibrary.Blocks, DifferentialEquations, LinearAlgebra, Rotations, Plots
-using Symbolics: scalarize 
+using Symbolics: scalarize
 
 @variables t
 D = Differential(t) 
@@ -20,18 +20,16 @@ RigidBody
     outputs:
 """
 function RigidBody(;i=1000.0*I(3), θ0 = zeros(3), ω0 = zeros(3), name)
-    @named input_T = RealInput(u_start = zeros(3))
-    @parameters i[1:3,1:3]
-    @variables θ(t)[1:3] = θ0
-    @variables ω(t)[1:3] = ω0
+    @named input_T = RealInput(u_start = zeros(3), nin = 3)
+    i = scalarize(only(@parameters i[1:3,1:3] = i))
+    θ, ω = scalarize.(@variables(θ(t)[1:3] = θ0, ω(t)[1:3] = ω0))
 
-    T = input_T.u
+    T = scalarize(input_T.u)
     eqs = [
-        scalarize(D.(θ) .~ ω)
-        scalarize(D.(ω) .~ inv(scalarize(i))*(scalarize(T).-scalarize(cross(ω,i*ω)))) #gotta be a better way than scalarizing everything independently, but this works for now
-        # D.(ω) .~ inv(I)*(T-h-İ*ω-cross(ω,I*ω))
+        D.(θ) .~ ω
+        D.(ω) .~ inv(i) * (T - ω×(i*ω))
     ]
-    return compose(ODESystem(eqs, t, [θ...;ω...], [i...];name = name), input_T)
+    return compose(ODESystem(eqs; name = name), input_T)
 end
 
 """
@@ -46,13 +44,14 @@ Thruster
     outputs:
         T - resultant external torque in reference frame
 """
-function Thruster(Fmag=1.0,R=[1.0,0,0],rotation=1.0*I(3);name) 
-    @named output_T = RealOutput(u_start = zeros(3))  
+function Thruster(Fmag=1.0, R=[1.0,0,0], rotation=1.0*I(3);name) 
+    @named output_T = RealOutput(u_start = zeros(3), nout = 3)  
     @named input_u = RealInput() 
-    @parameters Fmag, R[1:3], F[1:3], rotation[1:3,1:3]
+    Fmag, R, rotation = scalarize.(@parameters Fmag=Fmag R[1:3]=R rotation[1:3,1:3] = rotation)
 
-    eqs = scalarize(output_T.u .~ input_u.u*cross(R,rotation*[Fmag,0,0]))
-    compose(ODESystem(eqs,t,[], [Fmag,R...,F...,rotation...],name=name),output_T,input_u)
+    F = [Fmag, 0, 0]
+    eqs = scalarize(output_T.u) .~ scalarize(input_u.u * (R × (rotation*F)))
+    return compose(ODESystem(eqs, name=name), output_T, input_u)
 end
 
 """
@@ -62,19 +61,24 @@ Make connections to make sys
 @named th = Thruster()
 @named command = Step(start_time = 3.0, duration = 1.0)
 @named sys = ODESystem([
-    connect(rb.input_T, th.output_T)
-    connect(command.output,th.input_u)
-    ],t,systems = [rb,th])
+        connect(rb.input_T, th.output_T)
+        connect(command.output,th.input_u)],
+    t,
+    systems = [rb, th, command],
+)
+simplified_sys = structural_simplify(sys)
 
 
 """
 Test function to verify results
 """
 function test(sys)
-prob = ODEProblem(sys, Pair[], (0.0,10.0))
-sol = solve(prob)
-return sol
+    prob = ODEProblem(sys, Pair[], (0.0,10.0))
+    sol = solve(prob)
+    return sol
 end
+
+test(simplified_sys)
 
 """
 TBD
