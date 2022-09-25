4a97d6a8db152b6de7c80c07edcbef6b131a07b1
jpdavis6 <jonathan.p.davis@nasa.gov>
2022-09-25 01:11:25 -0400

    Added an ODESystem ReactionWheel(). Model is simple and needs improved fidelity, but runs reasonably. Code likely needs cleanup, and the equations could be simplified.

--- jsat_mtk.jl
+++ jsat_mtk.jl
@@ -36,60 +36,99 @@ end
 Thruster 
     states:
     parameters:
-        Fmag - magnitude of the thruster force
+        F - magnitude of the thruster force
         R - position vector to thruster location in reference frame
-        rotation - DCM from thruster line of action to reference frame (TODO: maybe use rotations.jl one day)
+        θ - DCM from thruster line of action to reference frame (TODO: maybe use rotations.jl one day)
     inputs:
         u - thruster command (0 = off, 1 = on)
     outputs:
         T - resultant external torque in reference frame
 """
-function Thruster(Fmag=1.0, R=[1.0,0,0], rotation=1.0*I(3);name) 
+function Thruster(F=100.0, R=[0.0,5.0,0.0], θ=1.0*I(3);name) 
     @named output_T = RealOutput(u_start = zeros(3), nout = 3)  
     @named input_u = RealInput() 
-    Fmag, R, rotation = scalarize.(@parameters Fmag=Fmag R[1:3]=R rotation[1:3,1:3] = rotation)
+    F, R, θ = scalarize.(@parameters F=F R[1:3]=R θ[1:3,1:3] = θ)
 
-    F = [Fmag, 0, 0]
-    eqs = scalarize(output_T.u) .~ scalarize(input_u.u * (R × (rotation*F)))
+    F̄ = [F, 0, 0]
+    eqs = scalarize(output_T.u) .~ scalarize(input_u.u * (R × (θ*F̄)))
     return compose(ODESystem(eqs, name=name), output_T, input_u)
 end
 
 """
+ReactionWheel   
+    states:
+        ωs - wheel speed
+
+    parameters:
+        kt - motor torque constant
+        r - location to wheel cg
+        θ - rotation from wheel frame to reference frame, wheel assumed to spin about wheel frame x axis
+        i - wheel inertia
+        TODO: Wheel equations, what parameters do I need?
+    inputs:
+        input_u - current
+    outputs:
+        h - internal momentum in the reference frame
+        ḣ - internal torque
+"""
+function ReactionWheel(;i = 0.25 ,kt = 1,θ = I(3),ωs0 = 100*2*pi/60,name)
+#0.231 kg m^2
+    unit_vec = θ*[1,0,0]
+    h0 = i*ωs0.*unit_vec
+    @named input_u = RealInput() 
+    @named output_T = RealOutput(u_start = zeros(3), nout = 3)
+    @named output_h = RealOutput(u_start = zeros(3), nout = 3)
+    ωs, h = scalarize.(@variables(ωs(t) = ωs0, h(t)[1:3] = h0))
+    kt,i,θ = scalarize.(@parameters kt=kt i=i θ=θ)
+
+    Tm = kt*input_u.u
+    T = Tm*unit_vec
+    h = i*ωs*unit_vec
+    eqs = [
+        D.(ωs) ~ Tm/i    
+        scalarize(output_T.u) .~ scalarize(T)
+        scalarize(output_h.u) .~ scalarize(h)
+    ]
+    return compose(ODESystem(eqs, name=name), output_T, output_h, input_u)
+end
+"""
 Make connections to make sys
 """
 @named rb = RigidBody()
-@named th = Thruster()
-@named command = Step(start_time = 3.0, duration = 1.0)
-@named sys = ODESystem([
-        connect(rb.input_T, th.output_T)
-        connect(command.output,th.input_u)],
+@named thr = Thruster()
+@named thr_command = Step(start_time = 3.0, duration = 1.0)
+@named thr_sys = ODESystem([
+        connect(rb.input_T, thr.output_T)
+        connect(thr_command.output,thr.input_u)],
     t,
-    systems = [rb, th, command],
+    systems = [rb, thr, thr_command],
 )
-simplified_sys = structural_simplify(sys)
+thr_sys_s = structural_simplify(thr_sys)
+
+@named rw_command = Step(start_time = 3.0, duration = 1.0)
+@named rw = ReactionWheel()
+@named rw_sys = ODESystem([
+        connect(rw_command.output,rw.input_u)],
+    t,
+    systems = [rw, rw_command],
+)
+
+rw_sys_s = structural_simplify(rw_sys)
 
 
 """
 Test function to verify results
 """
+
 function test(sys)
     prob = ODEProblem(sys, Pair[], (0.0,10.0))
     sol = solve(prob)
     return sol
 end
 
-test(simplified_sys)
-
 """
 TBD
 """
-#thrusters_on = [3.0] => [T[1] ~ 100]
-#thrusters_off = [4.0] => [T[1] ~ 0]
-#thrusters = [thrusters_on, thrusters_off]
-
-
-
-
 #inertia = [
 #    669.42  65.120  15.613
 #    65.120  1431.23 -39.827
