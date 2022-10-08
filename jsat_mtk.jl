51251304b85078fb1571c8786793ae503a1d18f2
jpdavis6 <jonathan.p.davis@nasa.gov>
2022-10-08 19:36:12 -0400

    Still working to get rw working. Had them working correctly but something else went wrong. Going upstairs to keep working.

--- jsat_mtk.jl
+++ jsat_mtk.jl
@@ -102,32 +102,49 @@ function make(component::Vector{ReactionWheel})
     H_tmp = []
     Tm = []
     for i in 1:n
-        Tm = kt[i]*input_u.u[i]
-        push!(T_tmp,Tm*a[:,i])
+        push!(Tm,kt[i]*input_u.u[i])
+        push!(T_tmp,Tm[i]*a[:,i])
         push!(H_tmp,J[i]*ωs[i]*a[:,i])
     end
-    if n == 1
-        T = scalarize(T_tmp)
-        H = scalarize(H_tmp)
-    else
-        T = scalarize(sum(T_tmp))
-        H = scalarize(sum(H_tmp))
-    end
-    print(T)
-    eqs = [
+    T = sum(T_tmp)
+    H = sum(H_tmp)
+    eqs = scalarize([
         D.(ωs) .~ Tm./J    
-        0 .~ output_T.u - T
-        0 .~ output_H.u - H
+        output_T.u .~ T
+        output_H.u .~ H
+    ])
+    return compose(ODESystem(eqs, name=:rw), output_T, output_H, input_u)
+end
+
+function make(component::ReactionWheel)
+    @named input_u = RealInput(u_start = 0., nin = 1) 
+    @named output_T = RealOutput(u_start = zeros(3), nout = 3)
+    @named output_H = RealOutput(u_start = zeros(3), nout = 3)
+    @variables ωs(t) = component.ωs0
+    @parameters begin
+        kt = component.kt
+        J = component.J
+        a[1:3] = component.a
+    end    
+    Tm = kt*input_u.u
+    T = Tm*a
+    H = J*ωs*a
+    
+    eqs = [
+        D.(ωs) ~ Tm/J    
+        scalarize(output_T.u .~ T)
+        scalarize(output_H.u .~ H)
     ]
     return compose(ODESystem(eqs, name=:ReactionWheels), output_T, output_H, input_u)
 end
 
-function make(component::ReactionWheel)
-    make([component])
-end
+"""
+Step function for unit testing
+    n - number of step functions out put as an array (single output)
+"""
 
-function nStep(times,durations,values;name)
-    n = 3
+function nStep(;name,times,durations,values)
+    n = length(times)
     @named output_u = RealOutput(u_start = zeros(n), nout = n)
     @parameters begin
         times[1:n] = times
@@ -140,10 +157,8 @@ function nStep(times,durations,values;name)
     end
     return compose(ODESystem(eqs, t, name=name), [output_u])
 end
-"""
-Test function to verify results
-"""
 
+#This was made to be differentiable but looks like nstep works, is simpler, and more flexible
 function Step3(;name,times,durations = [1.,1.,1.])
     @named s1 = Step(start_time=times[1], duration=durations[1],smooth = true)
     @named s2 = Step(start_time=times[2], duration=durations[2],smooth = true)
@@ -153,59 +168,53 @@ function Step3(;name,times,durations = [1.,1.,1.])
     return compose(ODESystem(eqs, name=name),s1,s2,s3,output)  
 end
 
+"""
+Test function to verify results
+"""
+
 function test(sys)
     prob = ODEProblem(sys, Pair[], (0.0,10.0), check_length = false)
     sol = solve(prob)
     return sol
 end
 
+
+"""
+Example systems
+"""
+
+#Make the Rigid Body
+@named rb = RigidBody()
+
+#Make the Reaction Wheels
 r1 = ReactionWheel(0.25, 1, [ 1, 0, 0], 20)
 r2 = ReactionWheel(0.35, 1.1, [ 0, 1, 0], 30)
 r3 = ReactionWheel(0.45, 1.2, [ 0, 0, 1], 40)
-
 rw = make([r1,r2,r3])
 
-@named rw_command = nStep([1.,4.,7.], [1.,1.,1.], [1.,1.,1.])
+#@named rw_command = nStep(times = [1.,4.,7.], durations = [1.,1.,1.], values = [1.,1.,1.])
+@named rw_command = Step3(times = [1.,4.,7.])
+@named rw_sys = ODESystem([
+        connect(rw_command.output,rw.input_u)
+    ], t, systems = [rw,rw_command] )
 
-@named sys = ODESystem([connect(rw.input_u, rw_command.output_u)],t,systems = [rw,rw_command])
-
-sys_s = structural_simplify(sys)
-
-"""
-Make connections to make sys
-"""
-#=
-@named rb = RigidBody()
-
+    #=
+#Make the Thrusters
 @named thr = Thruster()
-@named thr_command = Step(start_time = 20.0, duration = 1.0)
+@named thr_command = Step(start_time = 9, duration = 0.5)
 @named thr_sys = ODESystem([
         connect(thr_command.output,thr.input_u)
-        ],
-    t, systems = [thr, thr_command])
-thr_sys_s = structural_simplify(thr_sys)
+        ], t, systems = [thr, thr_command])
 
-@named rw_command = Step(start_time = 3.0, duration = 1.0)
-@named rw = ReactionWheel()
-@named rw_sys = ODESystem([
-        connect(rw_command.output,rw.input_u)    
-        ],
-    t, systems = [rw, rw_command])
-rw_sys_s = structural_simplify(rw_sys)
+@named fake_thrusters = nStep(times = [Inf,Inf,Inf], durations = ones(3),values = ones(3)) 
+
+# Connect components to Rigid Body
 
 @named sys = ODESystem([
-    connect(rw_sys.rw.output_H,rb.input_Hi)
-    connect(rw_sys.rw.output_T,rb.input_Ti)
-    connect(thr_sys.thr.output_T,rb.input_T)
-], t, systems = [rw_sys, thr_sys, rb])
+    connect(rw_sys.ReactionWheels.output_T, rb.input_Ti)
+    connect(rw_sys.ReactionWheels.output_H, rb.input_Hi)
+    connect(fake_thrusters.output_u, rb.input_T)
+    ],t,systems = [rw_sys,fake_thrusters,rb])
 
 sys_s = structural_simplify(sys)
 =#
-"""
-TBD
-"""
-#inertia = [
-#    669.42  65.120  15.613
-#    65.120  1431.23 -39.827
-#    15.613  -39.827 1551.76 
-#]
